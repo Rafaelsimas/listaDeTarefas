167a510877951c87ad40379d760842be6ecf879b
Rafael Simas <rafael.simas@al.resilia.com.br>
2025-04-17 22:03:58 -0300

    Atualização do joão

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import img_logo from "./assets/logo2-removebg-preview 1.png"
 import "./App.css"
 export default function App() {
-  return(
+  return (
     <div className="home">
       <img src={img_logo} alt="" className="logo" />
+      {/* Botão do Felipe */}
     </div>
   )
 }
