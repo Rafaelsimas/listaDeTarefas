4a7ddd4f28e6fb69706937d0eced0033446b8ae6
Rafael Simas <rafael.simas@al.resilia.com.br>
2025-04-18 21:44:08 -0300

    Adicionada a tela de apresentação

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,3 +1,22 @@
+import Menu from "../Menu/Menu"
+import logoDashboard from "../../assets/logoDashboard.svg"
+import { Link } from "react-router-dom"
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+  return (
+    <>
+      <Menu />
+      <div className="containerDashboard">
+        <img src={logoDashboard} alt="" />
+        <div className="container-txt">
+          <div className="txt-xl ubuntu-bold">Organize suas tarefas</div>
+          <div className="txt-md ubuntu-regular">
+            Mais produtividade no seu dia.
+          </div>
+          <Link to="/aplicacao" className="btn-dashboard ubuntu-bold">
+            Entrar
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
