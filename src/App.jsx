591171c658f5aec5d2654c9f5accd84faf802485
FeliperSanto5 <felipersantos4@gmail.com>
2025-04-18 22:48:57 -0300

    Criada parte inferior e caixa de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "./Components/Dashboard/Dashboard"
 import Explorer from "./Components/Explorer/Explorer"
 import Menu from "./Components/Menu/Menu"
-
+import Aplication from "../src/Components/Aplication/Aplication"
 export default function App() {
   return (
     <>
@@ -10,6 +10,7 @@ export default function App() {
         <Routes>
           <Route path="/" exact element={<Explorer />} />
           <Route path="/lista" exact element={<Dashboard />} />
+          <Route path="/aplicacao" exact element={<Aplication />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Aplication/Aplication.jsx
+++ src/Components/Aplication/Aplication.jsx
@@ -1,3 +1,25 @@
+import { FaTrashAlt } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
 export default function Aplication() {
-  return <div className="container"></div>
+  return <div className="container">
+    {/* Felipe */}
+<div className="task-bx">
+  <p className="count">Você tem 0 tarefas</p>
+
+  <div className="task-card">
+    <div className="title">Estudar React
+    <div className="icon">
+    <FaTrashAlt />
+    <FaPen />    
+    </div>
+    </div>
+
+
+    <div className="description">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia hic fuga quibusdam ipsum tempore. Ad consequuntur saepe impedit error quo eum aliquid officiis! Maiores iste corporis totam, pariatur cum aperiam?
+    </div>
+  </div>
+</div>
+
+  </div>
 }
