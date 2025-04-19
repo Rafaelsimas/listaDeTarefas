80742e17cfc1993c6d93cc1e1a8c1b0eb1610850
Jv12102008 <Jvbonfim13256@gmail.com>
2025-04-18 22:49:10 -0300

    Adicionado a função (Date)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "./Components/Dashboard/Dashboard"
 import Explorer from "./Components/Explorer/Explorer"
 import Menu from "./Components/Menu/Menu"
+import Aplication from "./Components/Aplication/Aplication"
 
 export default function App() {
   return (
     <>
+  
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Explorer />} />
           <Route path="/lista" exact element={<Dashboard />} />
+          <Route path="/aplicacao" exact element={<Aplication />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Aplication/Aplication.jsx
+++ src/Components/Aplication/Aplication.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import Menu from "../Menu/Menu";
+import { IoIosAddCircle } from "react-icons/io";
+
 export default function Aplication() {
-  return <div className="container"></div>
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000); // atualiza a cada 1 segundo
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatted = now.toLocaleString("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  return (
+    <>
+      <Menu />
+      <div className="container">
+        <div className="date">
+          <h1>{formatted}</h1>
+        </div>
+          <IoIosAddCircle  className="add"/>
+      </div>
+    </>
+  );
 }
